16c63758f7fb44802003fd5ac4c9f68c727bc446
Parthiban237 <parthibannatarajan03@gmail.com>
2026-08-19 19:41:36 +0530

    connect the code

---
+++ data/cart.js
@@ -0,0 +1,41 @@
+export let cart=[{
+    productId:'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
+    quantity:2,
+},{
+    productId:'15b6fc6f-327a-4ec4-896f-486349e85a3d',
+    quantity:1,
+}];
+
+function saveToString(){
+  localStorage.setItem('cart',JSON.stringify(cart));
+}
+export function addToCart(productId){
+  let matchingitem;
+      cart.forEach((cartItem) => {
+        if(productId === cartItem.productId){
+          matchingitem = cartItem;
+        }
+      })
+
+      if(matchingitem){
+        matchingitem.quantity += 1;
+      }else{
+      cart.push({
+        productId:productId,
+        quantity:1
+      }
+    )}
+    saveToString();
+}
+
+export function removeFromCart(productId){
+  let newCart= [];
+  cart.forEach((cartItem)=>{
+    if(cartItem.productId !== productId){
+      newCart.push(cartItem);
+    }
+  });
+
+  cart = newCart;
+  saveToString();
+}
